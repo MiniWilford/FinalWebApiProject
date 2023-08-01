3c087397ca3181dc1bf4a11aa39957fd3d5be6ba
MiniWilford <ktbtut@gmail.com>
2023-08-01 10:29:15 -0700

    Serperated Model files for database

--- main/Models/Movies.cs
+++ main/Models/Movies.cs
@@ -6,20 +6,3 @@ public class Movies
     public string Director {get; set;}
     public int Year {get; set;} 
 }
-
-public class Students
-{
-    public int StudentId { get; set; }
-    public string StudentName { get; set; }
-    public DateTime? DateoOfBirth { get; set; }
-    public string StudentProgram { get; set; }
-    public int StudentYear { get; set; }
-}
-
-public class Food
-{
-    public int FoodId { get; set; }
-    public string FoodName { get; set;}
-    public string FoodDescription { get; set; }
-    public string FoodType { get; set;}
-}
