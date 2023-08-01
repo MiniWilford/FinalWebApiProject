c200a48dcbbd8233638eddb13e46c9394be8c758
Beyershe <133257978+Beyershe@users.noreply.github.com>
2023-08-01 13:58:24 -0400

    Run and Debug Files Added

--- main/Data/MovieContext.cs
+++ main/Data/MovieContext.cs
@@ -11,7 +11,7 @@ namespace Main.Data
            builder.Entity<Movies>().HasData(
                 new Movies{Id = 1, Title = "Halloween", Genre = "Horror", Director = "John Carpenter", Year = 1978},
                 new Movies{Id = 2, Title = "Mask", Genre = "Comedy", Director = "Charles Russel", Year = 1994},
-                new Movies{Id =3, Title = "John Wick", Genre = "Action", Director = "Chad Stahelski", Year = 2014},
+                new Movies{Id = 3, Title = "John Wick", Genre = "Action", Director = "Chad Stahelski", Year = 2014},
                 new Movies{Id = 4, Title = "Hellboy", Genre = "Fantasy/Action", Director = "Guillermo del Toro", Year = 2004},
                 new Movies{Id = 5, Title = "The Shawshank Redemption", Genre = "Drama", Director = "Frank Darabont", Year = 1994}
            );
