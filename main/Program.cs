a954cb74e276e56847e31b6e0a7494e97a9e3883
Beyershe <133257978+Beyershe@users.noreply.github.com>
2023-08-01 14:31:46 -0400

    Added connection strings for movie and book. Added NSwag

--- main/Program.cs
+++ main/Program.cs
@@ -1,7 +1,20 @@
+using Main.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSwaggerDocument();
+
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("MovieConnection")
+));
+builder.Services.AddDbContext<BookContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("BookConnection")
+));
+
 
 var app = builder.Build();
 
@@ -12,6 +25,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseOpenApi();
+app.UseSwaggerUi3();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
